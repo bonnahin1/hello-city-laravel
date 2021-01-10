28fd770b0ab9c516d1a561362ccaf081e134605a
dahoneric <dahoneric3@gmail.com>
2021-01-10 05:55:55 +0100

    Add propper page title

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,7 +1,9 @@
 @extends('app')
 
+@section('title', 'About Us | Hello City')
+
 @section('content')
 <p>Built with &hearts; by Bonn@hin.</p>
 
 <p><a href="/">Revenir à la page d'accueil</a></p>
-@endsection
+@stop
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,4 +4,4 @@
 <h1>Hello from Quebec!</h1>
 
 <p>It's curently {{ date('h:i A') }}.</p>
-@endsection
+@stop
