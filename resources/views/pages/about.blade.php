e44e141c555edec2825e07ed2d6d4cc06e19b8c2
dahoneric <dahoneric3@gmail.com>
2021-01-10 05:22:46 +0100

    Remove duplication by using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,22 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('content')
+<p>Built with &hearts; by Bonn@hin.</p>
 
-    <title>About Us | Hello City</title>
-
-</head>
-
-<body>
-    <p>Built with &hearts; by Bonn@hin.</p>
-
-    <p><a href="/">Revenir à la page d'accueil</a></p>
-
-    <footer>
-        <p>&copy; Copyright {{ date('Y') }} &middot;</a></p>
-    </footer>
-</body>
-
-</html>
+<p><a href="/">Revenir à la page d'accueil</a></p>
+@endsection
