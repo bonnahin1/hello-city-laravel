69675a6db8fac4f36f5e04effe6269d19ff3fad9
dahoneric <dahoneric3@gmail.com>
2021-01-10 11:14:08 +0100

    Add styling with Tailwind CSS

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,9 +3,13 @@
 @section('title', 'About Us | ' .config('app.name'))
 
 @section('content')
-<img src="{{ asset('images/tdn.png') }}" alt="TDN Logo">
+<img src="{{ asset('images/tdn.png') }}" alt="TDN Logo" class="my-12 rounded-full shadow-md">
 
-<p>Built with &hearts; by Bonn@hin.</p>
+<h2 class="mb-5 text-gray-700">
+    Built with <span class="text-pink-500">&hearts;</span> by Bonn@hin.
+</h2>
 
-<p><a href="{{ route('home') }}">Revenir à la page d'accueil</a></p>
+<p class="">
+    <a href="{{ route('home') }}" class="text-indigo-500 hover:text-indigo-600 underline">Revenir à la page d'accueil</a>
+</p>
 @stop
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,9 +1,9 @@
 @extends('app')
 
 @section('content')
-<img src="{{ asset('images/casa.png') }}" alt="casablanca Flag">
+<img src="{{ asset('images/casa.png') }}" alt="casablanca Flag" class="mt-12 rounded shadow-md h-32">
 
-<h1>Hello from Casablanca!</h1>
+<h1 class="mt-5 text-3xl sm:text-5xl font-semibold text-indigo-600">Hello from Casablanca!</h1>
 
-<p>It's curently {{ date('h:i A') }}.</p>
+<p class="text-lg text-gray-800">It's curently {{ date('h:i A') }}.</p>
 @stop
